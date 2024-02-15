373248b6246b56b1b48f27050c5cc5802d2297f6
unknown <gjosemanuel469@gmail.com>
2024-02-15 13:11:30 -0500

    first commit

---
+++ src/main/java/com/authorizationapi/authorizationapi/AuthorizationapiApplication.java
@@ -0,0 +1,13 @@
+package com.authorizationapi.authorizationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authorizationapi/authorizationapi/AuthorizationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authorizationapi.authorizationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
